c4461fc0f91a61f3dab35dd36d661c91c4479ca4
Patrick Mombleau <6268139@collegemv.qc.ca>
2024-02-13 17:34:56 -0500

    Ajout fonction checkbox

--- Accueil/script.js
+++ Accueil/script.js
@@ -6,23 +6,32 @@ var i = 0;
 
 addTaskBtn.addEventListener("click", addTask);
 
-
-
 function addTask() {
     const taskText = taskInput.value.trim();
 
     if (taskText !== "") {
         const listItem = document.createElement("li");
+
         listItem.textContent = taskText;
         taskList.appendChild(listItem);
         taskInput.value ="";
 
-        const deleteBtn = document.createElement("button");
         const checkBox = document.createElement("input");
         checkBox.setAttribute("type", "checkbox");
-        listItem.appendChild(checkBox);
+        listItem.prepend(checkBox);
+
+        checkBox.addEventListener("change", () => {
+            if (checkBox.checked) {
+                listItem.setAttribute("style", "text-decoration-line: line-through;")
+            } else {
+                listItem.setAttribute("style", "text-decoration-line: none;")
+            }
+        });
+
+        const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "Supprimer";
         listItem.appendChild(deleteBtn);
+
         deleteBtn.addEventListener("click", () => {
             listItem.remove();
         });
